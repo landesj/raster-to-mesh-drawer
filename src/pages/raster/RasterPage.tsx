0cb3ca7bc8f271730684e72b66fdc277ada70213
jeffrey-landes-adsk <jeffrey.landes@autodesk.com>
2022-01-30 21:33:14 -0500

    feat: add map to raster page

--- src/pages/raster/RasterPage.tsx
+++ src/pages/raster/RasterPage.tsx
@@ -1,10 +1,36 @@
 import { useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import styled from "styled-components";
+import "leaflet/dist/leaflet.css";
 
 const Page = styled.div`
   display: flex;
   flex-direction: column;
 `;
+
+function LeafletMap() {
+  return (
+    <div>
+      <h2>This is a map.</h2>
+      <MapContainer
+        style={{ height: "800px" }}
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
 
 export function RasterPage() {
   const [rasterState, setRasterState] = useState<ArrayBuffer | null>(null);
@@ -16,14 +42,12 @@ export function RasterPage() {
       reader.onerror = () => {
         alert("Encountered an error when reading file.");
       };
-      reader.onload = (file) => {
-        setRasterState(file.target?.result as ArrayBuffer);
-      };
       reader.readAsArrayBuffer(file);
+      reader.onloadend = () => {
+        setRasterState(reader.result as ArrayBuffer);
+      };
     }
   };
-
-  console.log(rasterState);
 
   return (
     <Page>
@@ -33,6 +57,9 @@ export function RasterPage() {
         accept=".tif,.tiff"
         onChange={(event) => onChange(event.target.files)}
       />
+      <div id="map" style={{ height: "180px" }}>
+        <LeafletMap />
+      </div>
     </Page>
   );
 }
