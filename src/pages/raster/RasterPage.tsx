f0e63d8208729e7f4c67237695fd28d208f287e4
jeffrey-landes-adsk <jeffrey.landes@autodesk.com>
2022-01-31 21:05:48 -0500

    feat: import raster and render new location on map

--- src/pages/raster/RasterPage.tsx
+++ src/pages/raster/RasterPage.tsx
@@ -1,35 +1,29 @@
-import { useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useRef, useState } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
 import styled from "styled-components";
 import "leaflet/dist/leaflet.css";
+import { useMap } from "react-leaflet";
+import GeoRasterLayer from "georaster-layer-for-leaflet";
+import parseGeoraster from "georaster";
 
 const Page = styled.div`
   display: flex;
   flex-direction: column;
 `;
 
-function LeafletMap() {
-  return (
-    <div>
-      <h2>This is a map.</h2>
-      <MapContainer
-        style={{ height: "800px" }}
-        center={[51.505, -0.09]}
-        zoom={13}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
-  );
+type Props = { rasterArrayBuffer: ArrayBuffer | null };
+
+function RasterImport(props: Props) {
+  const leafletMap = useMap();
+
+  if (!props.rasterArrayBuffer) return <></>;
+
+  parseGeoraster(props.rasterArrayBuffer).then((georaster: any) => {
+    const geoTiff = new GeoRasterLayer({ georaster: georaster });
+    leafletMap.addLayer(geoTiff);
+    leafletMap.fitBounds(geoTiff.getBounds());
+  });
+  return <></>;
 }
 
 export function RasterPage() {
@@ -48,7 +42,6 @@ export function RasterPage() {
       };
     }
   };
-
   return (
     <Page>
       <h2>This is the Raster page.</h2>
@@ -57,8 +50,18 @@ export function RasterPage() {
         accept=".tif,.tiff"
         onChange={(event) => onChange(event.target.files)}
       />
-      <div id="map" style={{ height: "180px" }}>
-        <LeafletMap />
+      <div style={{ padding: "10px" }}>
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          style={{ height: "800px", width: "600px" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <RasterImport rasterArrayBuffer={rasterState} />
+        </MapContainer>
       </div>
     </Page>
   );
