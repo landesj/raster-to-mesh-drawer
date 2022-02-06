8d45557f3d439f10243a82af54a0b4e068ea480b
jeffreylandes <jeffrey@spacemaker.ai>
2022-02-06 13:46:55 -0500

    fix: connecting polygons by getting target div

--- src/pages/mesh/MeshPage.tsx
+++ src/pages/mesh/MeshPage.tsx
@@ -2,9 +2,31 @@ import { useState } from "react";
 import { debounce } from "lodash";
 import { useCallback } from "react";
 
-type point = { x: number; y: number };
+type PointType = { x: number; y: number };
+type LineType = { xSrc: number; ySrc: number; xDst: number; yDst: number };
 
-function Point({ x, y }: point) {
+function Line({ xSrc, ySrc, xDst, yDst }: LineType) {
+  return (
+    <svg
+      height="500"
+      width="500"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ position: "fixed" }}
+    >
+      <line
+        x1={xSrc}
+        y1={ySrc}
+        x2={xDst}
+        y2={yDst}
+        stroke="black"
+        strokeWidth={3}
+        style={{ position: "fixed", zIndex: 2 }}
+      />
+    </svg>
+  );
+}
+
+function Point({ x, y }: PointType) {
   return (
     <span
       style={{
@@ -15,6 +37,7 @@ function Point({ x, y }: point) {
         left: x - 5,
         top: y - 5,
         position: "fixed",
+        zIndex: 1,
       }}
     ></span>
   );
@@ -22,25 +45,46 @@ function Point({ x, y }: point) {
 
 export function MeshPage() {
   const [drawing, setDrawing] = useState<boolean>(false);
-  const [points, setPoints] = useState<point[]>([]);
+  const [points, setPoints] = useState<PointType[]>([]);
+  const [lines, setLines] = useState<LineType[]>([]);
+  const [latestPoint, setLatestPoint] = useState<PointType | undefined>();
 
   const setPointsDebounced = debounce(setPoints, 100);
+  const setLinesDebounced = debounce(setLines, 100);
+  const setLatestPointDebounced = debounce(setLatestPoint, 100);
 
   const addPoint = useCallback(
     (event) => {
       if (drawing) {
+        const divTarget = event.currentTarget.getBoundingClientRect();
         const newPoints = [...points, { x: event.clientX, y: event.clientY }];
+        const newPoint = {
+          x: event.clientX - divTarget.left,
+          y: event.clientY - divTarget.top,
+        };
         setPointsDebounced(newPoints);
+        if (latestPoint) {
+          const newLine = {
+            xSrc: latestPoint.x,
+            ySrc: latestPoint.y,
+            xDst: newPoint.x,
+            yDst: newPoint.y,
+          };
+          const newLines = [...lines, newLine];
+          setLinesDebounced(newLines);
+        }
+        setLatestPointDebounced(newPoint);
       }
     },
-    [points, drawing]
+    [points, drawing, latestPoint]
   );
 
   const onClick = () => {
     setDrawing(!drawing);
   };
   const buttonText = drawing ? "Stop Drawing" : "Start Drawing";
-  const drawnPoints = points.map((point: point) => Point(point));
+  const drawnPoints = points.map((point: PointType) => Point(point));
+  const drawnLines = lines.map((line: LineType) => Line(line));
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <h2>This is the Mesh page.</h2>
@@ -51,6 +95,7 @@ export function MeshPage() {
         onClick={addPoint}
       >
         {drawnPoints}
+        {drawnLines}
       </div>
     </div>
   );
